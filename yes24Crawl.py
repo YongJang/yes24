3eae1e39dc2fb19c03cecd1bd6531bb51bd4d0a2
YongJang <yj_430@naver.com>
2016-06-14 02:00:25 +0900

    test

--- yes24Crawl.py
+++ yes24Crawl.py
@@ -7,10 +7,7 @@ def Yes24_Crawling():
     n = 1
     html = Request('http://www.yes24.com/24/category/bestseller?CategoryNumber=001&sumgb=06&fetchSize=40&PageNumber=' + str(n), headers={'User-Agent':'Mozilla/5.0'})
     webpage = urlopen(html).read()
-    soup = BeautifulSoup(webpage , from_encoding="utf-8")
-    # //*[@id="category_layout"]/tbody/tr[1]/td[2]/div/a[1]
-    # //*[@id="category_layout"]/tbody/tr[3]/td[2]/div/a[1]
-    # //*[@id="category_layout"]/tbody/tr[5]/td[2]/div/a[1]
+    soup = BeautifulSoup(webpage, from_encoding="utf-8")
     bestsellers = soup.find_all("div" ,class_="goodsImgW")
     print(len(bestsellers))
     bestsellerLink = []
@@ -18,7 +15,14 @@ def Yes24_Crawling():
         if i.find_all("a") is not None :
             bestsellerLink.append('http://www.yes24.com/'+i.find("a").get("href"))
     for i in range(len(bestsellerLink)):
-        print(bestsellerLink[i])
+        print("페이지 : " + bestsellerLink[i])
+        detailhtml = Request(bestsellerLink[i])
+        detailpage = urlopen(detailhtml).read()
+        detailsoup = BeautifulSoup(detailpage, from_encoding="utf-8")
+        price = detailsoup.find("th", scope="row")
+        print(price)
+
+
 
 if __name__ == '__main__' :
     Yes24_Crawling()
