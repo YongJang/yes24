967b6776ed295fd5c11aa40d26626766e635bcd6
YongJang <yj_430@naver.com>
2016-06-14 02:11:15 +0900

    22

--- yes24Crawl.py
+++ yes24Crawl.py
@@ -19,7 +19,7 @@ def Yes24_Crawling():
         detailhtml = Request(bestsellerLink[i])
         detailpage = urlopen(detailhtml).read()
         detailsoup = BeautifulSoup(detailpage, from_encoding="utf-8")
-        price = detailsoup.find("th", scope="row").next_element
+        price = detailsoup.find("th", scope="row").next_sibling.next_element
         print(price)
 
 
